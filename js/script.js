2c9723c7759fabc06b0afdbedaf3cc586546fe40
Susanna Yossef <susannayossef98@gmail.com>
2021-12-06 16:28:48 +0100

    update

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ function buttonClicked(buttonName){
   const buttonTest = document.getElementById('button-test');
   buttonTest.addEventListener('click', titleClickHandler);
 
-  'use strict';
 function titleClickHandler(){
   const links = document.querySelectorAll('.titles a');
   console.log(links);
@@ -23,7 +22,6 @@ for(let link of links){
 console.log(link);
 }
 
-
 function titleClickHandler(){
   console.log('Link was clicked!');
 }
@@ -33,3 +31,40 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+'use strict';
+function titleClickHandler(event){
+  console.log('Link was clicked!');
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+  /* add class 'active' to the clicked link */
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.titles a.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classArticle.remove('active');
+}
+
+ /* get 'href' attribute from the clicked link */
+ /* find the correct article using the selector (value of 'href' attribute) */
+/* add class 'active' to the correct article */
+}
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
+sourceCapabilities: <inputDeviceCapabilities;
+srcElement: StereoPannerNode;
+target: Span;
+timeStamp:5530.530000000001;
+toElement: span;
+type:"click";
+
+
